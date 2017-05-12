d0b53421214553585eb7391a878822868e928d0b
Shanlly <j.fagen72@gmail.com>
2017-05-12 13:31:04 -0600

    cat page

---
+++ cats_server.js
@@ -0,0 +1,60 @@
+var http = require('http');
+var fs = require('fs');
+
+
+//create a server on my computer
+var server = http.createServer(function(req, res) {
+	console.log(req.url);
+	//dealing with incoming requests
+	if (req.url === "/") {
+		cats('./cat_page.html',res);
+		countCatVisits('./count_cat_visits.html',res);
+	} else if (req.url === "/hi") {
+		res.write("hi world");
+		res.end();
+	} else {
+		res.write("404 not found");
+		res.end();
+	}
+});
+
+//counts the number of visits to my cat webpage
+	function countCatVisits(fileName, res){
+		// read count_cat_visits and count the visits
+		fs.readFile(fileName, function(err, data){
+			var count= parseInt(data);
+			if(isNaN(count)){
+				count=0;
+			}
+			if(err){
+				console.log("Couldn't read file");
+				return;
+			}else {
+				count = count +1;
+			}
+		// write to the count_cat_visits
+			fs.writeFile(fileName, count, function(err){
+				if (err){
+					console.log("couldn't write file");
+				}
+			});
+		});
+		
+	}	
+
+// to deal with the cat page when called 
+	function cats(fileName, res){
+		fs.readFile(fileName,function(err, data){
+			if(err){
+				console.log("Couldn't read file.");
+				return;
+			}
+			res.write(data);
+			res.end();
+		});
+	}
+
+server.listen(8080);
+
+console.log("Server started http://localhost:8080");
+
---
+++ count_cat_visits.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var fs = require(fs);
+var count = 1;
+
+fs.readFile();
+fs.writeFile();
